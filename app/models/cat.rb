dae0f8ec0948a00db3de251196503ea7f5df8ddc
Jason Hinchcliffe <hinchy@gmail.com>
2017-05-09 13:05:21 +0000

    More gifs!

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -14,11 +14,14 @@ class Cat < ActiveRecord::Base
   
   def random_picture
     [
-      "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy.gif",
+      "http://i.imgur.com/PSAV8.gif",
       "http://media0.giphy.com/media/26tPf302sLnlgmQCc/giphy.gif",
       "http://media4.giphy.com/media/f2Y2yrwrUcr9C/giphy.gif",
       "http://i.imgur.com/TBNu0iI.gif",
-      "https://media0.giphy.com/media/3osxYaZU8dek0F9LVK/giphy.gif"
+      "https://media0.giphy.com/media/3osxYaZU8dek0F9LVK/giphy.gif",
+      "http://i.imgur.com/dEqJLlp.jpg",
+      "http://images.hellokids.com/_uploads/_tiny_galerie/20100207/glitter-paddy-dog-source_gx7.gif",
+      "http://heaven.internetarchaeology.org/ANIMALS/a1038.gif"
     ].shuffle.first
   end
   
